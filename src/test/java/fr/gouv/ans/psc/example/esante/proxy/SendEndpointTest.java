dc45dfbc7b334d37d7daf4680406418add454768
Eric Degenetais <edegenetais@henix.fr>
2024-10-17 17:43:50 +0200

    Add some second backend to make sure we do route requests.

--- src/test/java/fr/gouv/ans/psc/example/esante/proxy/SendEndpointTest.java
+++ src/test/java/fr/gouv/ans/psc/example/esante/proxy/SendEndpointTest.java
@@ -6,6 +6,7 @@ package fr.gouv.ans.psc.example.esante.proxy;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.matching.UrlPattern;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 public class SendEndpointTest {
 
   private static final int BAKCEND_1_PORT = 8081;
+  private static final int BAKCEND_2_PORT = 8082;
 
   @Autowired private WebTestClient testClient;
 
@@ -30,6 +32,12 @@ public class SendEndpointTest {
   static WireMockExtension backend1 =
       WireMockExtension.newInstance()
           .options(WireMockConfiguration.wireMockConfig().port(BAKCEND_1_PORT))
+          .build();
+  
+   @RegisterExtension
+  static WireMockExtension backend2 =
+      WireMockExtension.newInstance()
+          .options(WireMockConfiguration.wireMockConfig().port(BAKCEND_2_PORT))
           .build();
 
   @Test
@@ -46,5 +54,25 @@ public class SendEndpointTest {
         .isOk()
         .expectBody()
         .json(reponseBody);
+    
+    backend2.verify(WireMock.exactly(0), WireMock.anyRequestedFor(UrlPattern.ANY));
+  }
+  
+   @Test
+  public void getFromBackendTwo() {
+    final String reponseBody = "{\"status\": \"OK\"}";
+    backend2.stubFor(
+        WireMock.get(WireMock.urlEqualTo("/rsc2")).willReturn(WireMock.okJson(reponseBody)));
+
+    testClient
+        .get()
+        .uri("/send/backend-2/rsc2")
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectBody()
+        .json(reponseBody);
+
+    backend1.verify(WireMock.exactly(0), WireMock.anyRequestedFor(UrlPattern.ANY));
   }
 }
