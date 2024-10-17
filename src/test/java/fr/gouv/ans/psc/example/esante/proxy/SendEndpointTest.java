8c76227dc4963adfe817f45447cdf7320ceb4d24
Eric Degenetais <edegenetais@henix.fr>
2024-10-17 15:15:10 +0200

    Activating spring-gateway.

---
+++ src/test/java/fr/gouv/ans/psc/example/esante/proxy/SendEndpointTest.java
@@ -0,0 +1,31 @@
+/*
+ * (c) Copyright 2024-2024, Agence du Numérique en Santé (ANS) (https://esante.gouv.fr). All rights reserved.
+ */
+package fr.gouv.ans.psc.example.esante.proxy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+/**
+ * This test suite aims at testing the /send endpoint.
+ *
+ * @author edegenetais
+ */
+@SpringBootTest(classes = {EsanteProxyApplication.class})
+@AutoConfigureWebTestClient()
+public class SendEndpointTest {
+  @Autowired
+  private WebTestClient testClient;
+  
+  /**
+   * This test dies the second the first real life TC arises.
+   */
+  @Test
+  public void binnable(){
+    Assertions.assertTrue(true);
+  }
+}
