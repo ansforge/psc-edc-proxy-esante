521cda5ecd0246550f83acfb7abd4cb2e8de50d8
Eric Degenetais <edegenetais@henix.fr>
2024-10-21 11:51:53 +0200

    Un peu de documentation dans le code.

---
+++ src/main/java/fr/gouv/ans/psc/example/esante/proxy/model/package-info.java
@@ -0,0 +1,7 @@
+/*
+ * (c) Copyright 2024-2024, Agence du Numérique en Santé (ANS) (https://esante.gouv.fr). All rights reserved.
+ */
+/**
+ * Modèle utilisé par le proxy.
+ */
+package fr.gouv.ans.psc.example.esante.proxy.model;
